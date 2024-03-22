45a3a34be0ed1b66db0e1df0e41c8f2389a463ab
kayro-is <luffykosuki@gmail.com>
2024-03-22 11:51:55 +0100

    création de carrousel

--- script.js
+++ script.js
@@ -1,26 +1,50 @@
-// Initialise l'index de l'image actuellement visible à 0
-let index = 0;
+document.body.onload = function(){
+    nbr=5;
+    p=0;
 
-// Définit la fonction moveSlide, qui décale le carrousel d'un certain nombre de pas (step)
-function moveSlide(step) {
-    // Sélectionne toutes les images dans le carrousel en utilisant querySelectorAll et stocke dans 'slides'
-    const slides = document.querySelectorAll(".carousel-images img");
-    // Calcule le nombre total d'images dans le carrousel
-    const totalSlides = slides.length;
+    container = document.getElementById("container");
+    g = document.getElementById("g");
+    d = document.getElementById("d");
 
-    // Calcule le nouvel index en ajoutant le pas au index actuel,
-    // en utilisant le modulo pour s'assurer que l'index reste dans les limites de la liste des images
-    // Cela permet de boucler de la dernière image à la première et vice versa
-    index = (index + step + totalSlides) % totalSlides;
-    // Calcule la nouvelle valeur de transform pour décaler le carrousel, en utilisant l'index
-    // Multiplie l'index par -100 pour obtenir un décalage en pourcentage correspondant à la largeur d'une image
-    const newTransform = -index * 100 + "%";
+    container.style.width=(800*nbr) + "px";
 
-    // Sélectionne le conteneur des images du carrousel pour appliquer la transformation
-    const carouselImages = document.querySelector(".carousel-images");
-    // Applique la transformation pour décaler le carrousel à la nouvelle image
-    carouselImages.style.transform = `translateX(${newTransform})`;
+    for( i = 1 ; i <= nbr ; i++){
+
+        div = document.createElement("div");
+
+        div.className="photo";
+        div.style.backgroundImage="url('images/im"+i+".jpg')";
+
+        container.appendChild(div);
+    }
 }
 
-// Appelle moveSlide avec 0 pour initialiser le carrousel et afficher la première image
-moveSlide(0);
+g.onclick = function(){
+    if (p >-nbr+1)
+    p--;
+    container.style.transform="translate("+p*800+"px)";
+    container.style.transition="all 0.5s ease";
+    afficherMasquer();
+}
+
+d.onclick = function(){
+    if (p <0)
+    p++;
+    container.style.transform="translate("+p*800+"px)";
+    container.style.transition="all 0.5s ease";
+    afficherMasquer();
+}
+
+function afficherMasquer(){
+
+    if (p==-nbr+1)
+    g.style.visibility = "hidden";
+else
+    g.style.visibility = "visible";
+
+    if (p==0)
+
+    d.style.visibility = "hidden";
+else
+    d.style.visibility = "visible";
+}
