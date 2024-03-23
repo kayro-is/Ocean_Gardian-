599b430a23b124f191521728076cf66ae06a28af
kayro-is <luffykosuki@gmail.com>
2024-03-23 23:54:22 +0100

    jai supprimée le fichier connexion

--- connexion.js
+++ connexion.js
@@ -1,7 +1,2 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('search-btn').addEventListener('click', function() {
-      var form = document.getElementById('connexionForm');
-      form.style.display = form.style.display === 'none' ? 'block' : 'none';
-    });
-  });
+
   
