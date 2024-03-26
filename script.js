99f50a5ed63c22ae45a1770f8044d200f0b143f8
kayro-is <luffykosuki@gmail.com>
2024-03-26 23:53:19 +0100

    modification dans js

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ document.addEventListener("DOMContentLoaded",function() {
     {
 
         image :"./images/im1.jpg",
-        text : `<h1>Les baleines</h1> sont parmi les créatures marines les plus impressionnantes. Chaque année
+        text : `<h1>Les Baleines</h1> sont parmi les créatures marines les plus impressionnantes. Chaque année
         certaines espèces de baleines parcourent des milliers de kilomètres lors de leurs migrations
         Par exemple, la baleine à bosse peut voyager jusqu'à 25 000 kilomètres de l'Arctique à l'Antarctique 
         Ces migrations sont cruciales pour la survie de ces mammifères, et les scientifiques étudient activement
@@ -59,7 +59,7 @@ document.addEventListener("DOMContentLoaded",function() {
     },
     {
         image :"./images/im2.jpg",
-        text : `<h1>Les dauphins</h1> sont réputés pour leur intelligence et leur capacité à communiquer de manière complexe. <br>
+        text : `<h1>Les Dauphins</h1> sont réputés pour leur intelligence et leur capacité à communiquer de manière complexe. <br>
         Leurs clics, sifflements et vocalisations constituent un langage sophistiqué qui facilite la coordination dans la chasse,<br>
          la protection des petits, et même la transmission culturelle entre les groupes. Les chercheurs explorent ces formes de 
          communication pour mieux comprendre ..`,
@@ -67,7 +67,7 @@ document.addEventListener("DOMContentLoaded",function() {
     },
     {
         image :"./images/im3.jpg",
-        text : `<h1>Les récifs coralliens</h1> abritent une vie marine extrêmement diversifiée. 
+        text : `<h1>Les Récifs coralliens</h1> abritent une vie marine extrêmement diversifiée. 
         Ces écosystèmes sous-marins agissent comme des "jardins" extraordinaires,
          fournissant une maison à une variété de poissons, <br>
          de crustacés et d'autres organismes marins.
@@ -77,7 +77,7 @@ document.addEventListener("DOMContentLoaded",function() {
     },
     {
         image :"./images/im4.jpg",
-        text : `<h1>Les abysses océaniques</h1> abritent une multitude de créatures bioluminescentes qui 
+        text : `<h1>Les Abysses océaniques</h1> abritent une multitude de créatures bioluminescentes qui 
         émettent de la lumière pour attirer leurs proies ou se camoufler. Des poissons aux 
         méduses, ces créatures étonnantes sont adaptées à un environnement où la lumière du soleil 
         ne pénètre pas. Les scientifiques explorent encore ces mystérieux fonds marins pour découvrir 
@@ -86,7 +86,7 @@ document.addEventListener("DOMContentLoaded",function() {
     },
     {
         image :"./images/im5.jpg",
-        text : `<h1>Les requins</h1> occupent les océans de notre planète depuis plus de 400 millions d'années, 
+        text : `<h1>Les Requins</h1> occupent les océans de notre planète depuis plus de 400 millions d'années, 
         bien avant l'apparition des dinosaures sur terre. Ces prédateurs marins, souvent mal compris et 
         représentés comme de simples machines à tuer, jouent en réalité un rôle crucial dans l'équilibre
          des écosystèmes marins. Loin d'être les monstres que l'on dépeint souvent, les requins sont des 
@@ -96,7 +96,7 @@ document.addEventListener("DOMContentLoaded",function() {
     {
 
         image :"./images/im6.jpg",
-        text : `<h1>La mer Méditerranée</h1> encerclée par trois continents - Europe, Asie, et Afrique - est une étendue 
+        text : `<h1>La Mer Méditerranée</h1> encerclée par trois continents - Europe, Asie, et Afrique - est une étendue 
         d'eau salée d'une richesse et d'une complexité extraordinaires. Elle a été le berceau de nombreuses civilisations
          qui ont marqué l'histoire humaine, et elle continue d'être un carrefour culturel, économique, et écologique vital. 
          Mais la Méditerranée est bien plus qu'une simple mer; c'est un écosystème unique en son genre, abritant une biodiversité 
@@ -106,7 +106,7 @@ document.addEventListener("DOMContentLoaded",function() {
     {
 
         image :"./images/im7.jpg",
-        text : `<h1>Les orques</h1> souvent appelées baleines tueuses à cause de leur nom anglais "killer whales",
+        text : `<h1>Les Orques</h1> souvent appelées baleines tueuses à cause de leur nom anglais "killer whales",
          sont en réalité les plus grands membres de la famille des dauphins. Ces majestueux mammifères marins
           se distinguent par leur intelligence remarquable, leur comportement social complexe et leur répartition 
           géographique vaste, étant présents dans tous les océans du monde, des eaux glacées de l'Arctique et de 
