c63de3c2fc4207194ce0325ca429cf544a5f90c0
kayro-is <luffykosuki@gmail.com>
2024-03-27 23:04:09 +0100

    mise a jour responsive

--- script.js
+++ script.js
@@ -3,172 +3,134 @@ document.addEventListener("DOMContentLoaded", function() {
     const backBtn = document.getElementById("backBtn");
     const nextBtn = document.getElementById("nextBtn");
 
+    // Écouteur d'événement pour le bouton "Suivant"
+    nextBtn.addEventListener("click", function() {
+        scrollContainer.style.scrollBehavior = "smooth";
+        scrollContainer.scrollLeft += 800;
+    });
 
-})
-
-
-// Ajouter un écouteur d'événement sur le bouton "Suivant" pour faire défiler la galerie vers la droite
-nextBtn.addEventListener("click", () => {
-    scrollContainer.style.scrollBehavior = "smooth"; // Activer le défilement fluide pour une expérience utilisateur agréable
-    scrollContainer.scrollLeft += 800; 
-});
-
-// Ajouter un écouteur d'événement sur le bouton "Retour" pour faire défiler la galerie vers la gauche
-backBtn.addEventListener("click", () => {
-    scrollContainer.style.scrollBehavior = "smooth"; // Assurer que le défilement reste fluide lors du clic
-    scrollContainer.scrollLeft -= 800; // Faire défiler le conteneur de 900 pixels vers la gauche
-});
-
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    // Attendre que le DOM soit chargé
+    // Écouteur d'événement pour le bouton "Retour"
+    backBtn.addEventListener("click", function() {
+        scrollContainer.style.scrollBehavior = "smooth";
+        scrollContainer.scrollLeft -= 800;
+    });
 
     // Fonction pour créer des divs
     function creerDivs(nombre) {
         for (let i = 0; i < nombre; i++) {
-            // Créer une nouvelle div
             const nouvelleDiv = document.createElement("div");
-
-            // Ajouter une classe à la div
             nouvelleDiv.className = "myDiv";
-
-           
-        
-            // Ajouter la div à la nouvelle section
             document.querySelector(".gallery").appendChild(nouvelleDiv);
         }
     }
 
-    // Appeler la fonction avec le nombre de divs que vous souhaitez créer
-    creerDivs(7);
-});
-
-document.addEventListener("DOMContentLoaded",function() {
+    creerDivs(7); // Supposons que vous souhaitiez créer 7 divs
 
     const contenuDivs = [
-    {
-
-        image :"./images/im1.jpg",
-        text : `<h1>Les Baleines</h1> sont parmi les créatures marines les plus impressionnantes. Chaque année
-        certaines espèces de baleines parcourent des milliers de kilomètres lors de leurs migrations
-        Par exemple, la baleine à bosse peut voyager jusqu'à 25 000 kilomètres de l'Arctique à l'Antarctique 
-        Ces migrations sont cruciales pour la survie de ces mammifères, et les scientifiques étudient activement
-         ces comportements pour mieux comprendre `,
-        lien :""
-    },
-    {
-        image :"./images/im2.jpg",
-        text : `<h1>Les Dauphins</h1> sont réputés pour leur intelligence et leur capacité à communiquer de manière complexe. <br>
-        Leurs clics, sifflements et vocalisations constituent un langage sophistiqué qui facilite la coordination dans la chasse,<br>
-         la protection des petits, et même la transmission culturelle entre les groupes. Les chercheurs explorent ces formes de 
-         communication pour mieux comprendre ..`,
-        lien :""
-    },
-    {
-        image :"./images/im3.jpg",
-        text : `<h1>Les Récifs coralliens</h1> abritent une vie marine extrêmement diversifiée. 
-        Ces écosystèmes sous-marins agissent comme des "jardins" extraordinaires,
-         fournissant une maison à une variété de poissons, <br>
-         de crustacés et d'autres organismes marins.
-          Malheureusement, les récifs coralliens sont menacés par le changement climatique et d'autres activités humaines,
-           ce qui rend crucial leur protection et leur préservation.`,
-        lien :""
-    },
-    {
-        image :"./images/im4.jpg",
-        text : `<h1>Les Abysses océaniques</h1> abritent une multitude de créatures bioluminescentes qui 
-        émettent de la lumière pour attirer leurs proies ou se camoufler. Des poissons aux 
-        méduses, ces créatures étonnantes sont adaptées à un environnement où la lumière du soleil 
-        ne pénètre pas. Les scientifiques explorent encore ces mystérieux fonds marins pour découvrir 
-        de nouvelles espèces et comprendre les adaptations uniques de la vie marine des abysses..`,
-        lien :""
-    },
-    {
-        image :"./images/im5.jpg",
-        text : `<h1>Les Requins</h1> occupent les océans de notre planète depuis plus de 400 millions d'années, 
-        bien avant l'apparition des dinosaures sur terre. Ces prédateurs marins, souvent mal compris et 
-        représentés comme de simples machines à tuer, jouent en réalité un rôle crucial dans l'équilibre
-         des écosystèmes marins. Loin d'être les monstres que l'on dépeint souvent, les requins sont des 
-         créatures fascinantes qui méritent notre attention et notre respect.`,
-        lien :""
-    },
-    {
-
-        image :"./images/im6.jpg",
-        text : `<h1>La Mer Méditerranée</h1> encerclée par trois continents - Europe, Asie, et Afrique - est une étendue 
-        d'eau salée d'une richesse et d'une complexité extraordinaires. Elle a été le berceau de nombreuses civilisations
-         qui ont marqué l'histoire humaine, et elle continue d'être un carrefour culturel, économique, et écologique vital. 
-         Mais la Méditerranée est bien plus qu'une simple mer; c'est un écosystème unique en son genre, abritant une biodiversité 
-         exceptionnelle .`,
-        lien :""
-    },
-    {
-
-        image :"./images/im7.jpg",
-        text : `<h1>Les Orques</h1> souvent appelées baleines tueuses à cause de leur nom anglais "killer whales",
-         sont en réalité les plus grands membres de la famille des dauphins. Ces majestueux mammifères marins
-          se distinguent par leur intelligence remarquable, leur comportement social complexe et leur répartition 
-          géographique vaste, étant présents dans tous les océans du monde, des eaux glacées de l'Arctique et de 
-          l'Antarctique jusqu'aux mers plus tempérées et .`,
-        lien :""
-    },
-     
-    ];
-
-        // Fonction pour ajouter des images, du texte et des liens à des divs existantes
-
-
-        function addDivs() {
-            const divs = document.querySelectorAll(".myDiv");
+        {
     
-            // Pour chaque div, mettre à jour son contenu en utilisant les informations du tableau
-            divs.forEach((div, index) => {
-                const contenu = contenuDivs[index];
+            image :"./images/im1.jpg",
+            text : `<h1>Les Baleines</h1> sont parmi les créatures marines les plus impressionnantes. Chaque année
+            certaines espèces de baleines parcourent des milliers de kilomètres lors de leurs migrations
+            Par exemple, la baleine à bosse peut voyager jusqu'à 25 000 kilomètres de l'Arctique à l'Antarctique 
+            Ces migrations sont cruciales pour la survie de ces mammifères, et les scientifiques étudient activement
+             ces comportements pour mieux comprendre `,
+            lien :""
+        },
+        {
+            image :"./images/im2.jpg",
+            text : `<h1>Les Dauphins</h1> sont réputés pour leur intelligence et leur capacité à communiquer de manière complexe. <br>
+            Leurs clics, sifflements et vocalisations constituent un langage sophistiqué qui facilite la coordination dans la chasse,<br>
+             la protection des petits, et même la transmission culturelle entre les groupes. Les chercheurs explorent ces formes de 
+             communication pour mieux comprendre ..`,
+            lien :""
+        },
+        {
+            image :"./images/im3.jpg",
+            text : `<h1>Les Récifs coralliens</h1> abritent une vie marine extrêmement diversifiée. 
+            Ces écosystèmes sous-marins agissent comme des "jardins" extraordinaires,
+             fournissant une maison à une variété de poissons, <br>
+             de crustacés et d'autres organismes marins.
+              Malheureusement, les récifs coralliens sont menacés par le changement climatique et d'autres activités humaines,
+               ce qui rend crucial leur protection et leur préservation.`,
+            lien :""
+        },
+        {
+            image :"./images/im4.jpg",
+            text : `<h1>Les Abysses océaniques</h1> abritent une multitude de créatures bioluminescentes qui 
+            émettent de la lumière pour attirer leurs proies ou se camoufler. Des poissons aux 
+            méduses, ces créatures étonnantes sont adaptées à un environnement où la lumière du soleil 
+            ne pénètre pas. Les scientifiques explorent encore ces mystérieux fonds marins pour découvrir 
+            de nouvelles espèces et comprendre les adaptations uniques de la vie marine des abysses..`,
+            lien :""
+        },
+        {
+            image :"./images/im5.jpg",
+            text : `<h1>Les Requins</h1> occupent les océans de notre planète depuis plus de 400 millions d'années, 
+            bien avant l'apparition des dinosaures sur terre. Ces prédateurs marins, souvent mal compris et 
+            représentés comme de simples machines à tuer, jouent en réalité un rôle crucial dans l'équilibre
+             des écosystèmes marins. Loin d'être les monstres que l'on dépeint souvent, les requins sont des 
+             créatures fascinantes qui méritent notre attention et notre respect.`,
+            lien :""
+        },
+        {
     
-                // Ajouter une balise img avec une image
-                const newImg = document.createElement("img");
-                newImg.src = contenu.image;
-                newImg.alt = "image" + (index + 1);
-                div.appendChild(newImg);
+            image :"./images/im6.jpg",
+            text : `<h1>La Mer Méditerranée</h1> encerclée par trois continents - Europe, Asie, et Afrique - est une étendue 
+            d'eau salée d'une richesse et d'une complexité extraordinaires. Elle a été le berceau de nombreuses civilisations
+             qui ont marqué l'histoire humaine, et elle continue d'être un carrefour culturel, économique, et écologique vital. 
+             Mais la Méditerranée est bien plus qu'une simple mer; c'est un écosystème unique en son genre, abritant une biodiversité 
+             exceptionnelle .`,
+            lien :""
+        },
+        {
     
-                // Ajouter du texte à la div
-                const textDiv = document.createElement("p");
-                textDiv.innerHTML = contenu.text;
-                div.appendChild(textDiv);
-    
-                // Ajouter un lien à la div
-                const lienDiv = document.createElement("a");
-                lienDiv.href = contenu.lien;
-                lienDiv.innerHTML = "Lire la suite";
-                div.appendChild(lienDiv);
-            });
-        }
-    
-        addDivs();
-    });
+            image :"./images/im7.jpg",
+            text : `<h1>Les Orques</h1> souvent appelées baleines tueuses à cause de leur nom anglais "killer whales",
+             sont en réalité les plus grands membres de la famille des dauphins. Ces majestueux mammifères marins
+              se distinguent par leur intelligence remarquable, leur comportement social complexe et leur répartition 
+              géographique vaste, étant présents dans tous les océans du monde, des eaux glacées de l'Arctique et de 
+              l'Antarctique jusqu'aux mers plus tempérées et .`,
+            lien :""
+        },
+         
+        ];
 
+    // Fonction pour ajouter du contenu aux divs
+    function addDivs() {
+        const divs = document.querySelectorAll(".myDiv");
+        divs.forEach((div, index) => {
+            const contenu = contenuDivs[index];
+            const newImg = document.createElement("img");
+            newImg.src = contenu.image;
+            newImg.alt = `Image ${index + 1}`;
+            div.appendChild(newImg);
+            // Ajouter du texte à la div
+       
 
+            const textDiv = document.createElement("p");
+            textDiv.innerHTML = contenu.text;
+            div.appendChild(textDiv);
 
-    // déclancher une animation CSS au moment ou un élément devient visible a l'écran lors du scroll
-
-    document.addEventListener("DOMContentLoaded", () => {
-        // Cet observateur déclenchera une fonction de rappel chaque fois qu'un élément observé entre ou sort de la zone visible.
-        const observer = new IntersectionObserver((entries) => {
-            entries.forEach(entry => { // Correction ici: retrait de parenthèses inutiles autour de "entry"
-                if (entry.isIntersecting) {
-                    entry.target.classList.add("fadeInUp");
-                }
-            });
-        }, {
-            // L'option threshold définit quel pourcentage de l'élément doit être visible pour déclencher la fonction de rappel.
-            // Ici, dès que 10% de l'élément est visible, la fonction de rappel est déclenchée.
-            threshold: 0.1
+            const lienDiv = document.createElement("a");
+            lienDiv.href = contenu.lien;
+            lienDiv.textContent = "Lire la suite";
+            div.appendChild(lienDiv);
         });
-    
-        // Sélectionne tous les éléments h1 et h2 pour les observer.
-        const introSections = document.querySelectorAll("h1, h2");
-        introSections.forEach(section => {
-            observer.observe(section); // Démarre l'observation pour chaque section sélectionnée.
+    }
+
+    addDivs();
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("fadeInUp");
+            }
         });
+    }, { threshold: 0.1 });
+
+    const introSections = document.querySelectorAll("h1, h2");
+    introSections.forEach(section => {
+        observer.observe(section);
     });
+});
